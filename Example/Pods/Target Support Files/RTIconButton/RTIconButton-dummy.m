6f7932bd6bbe3853822768599a41a63b3e57900f
benfen <benfen@mogujie.com>
2016-03-30 18:32:37 +0800

    ignore

--- Example/Pods/Target Support Files/RTIconButton/RTIconButton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RTIconButton : NSObject
-@end
-@implementation PodsDummy_RTIconButton
-@end
--- Example/Pods/Target Support Files/RTIconButton/RTIconButton-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "RTIconButton.h"
-
-FOUNDATION_EXPORT double RTIconButtonVersionNumber;
-FOUNDATION_EXPORT const unsigned char RTIconButtonVersionString[];
-
